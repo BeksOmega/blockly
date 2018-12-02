c29df5c7c588b9523d389c0fbca55b2d86127eec
Beka Westberg <bekawestberg@gmail.com>
2018-12-02 07:12:55 -0800

    Added drawing w/ waits.Changed code to Interpreter. (#1)

--- generators/javascript/turtle.js
+++ generators/javascript/turtle.js
@@ -1,64 +1,67 @@
 Blockly.JavaScript['turtle_forward'] = function(block) {
   var value_forward = Blockly.JavaScript.valueToCode(block, 'FORWARD', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'turtle.forward(' + value_forward + ');\n';
+  var code = 'if (addWaits) {\n' +
+    '  waitForMillis(10);\n' +
+    '}\n' +
+    'forward(' + value_forward + ');\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_right'] = function(block) {
   var value_right = Blockly.JavaScript.valueToCode(block, 'RIGHT', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'turtle.right(' + value_right + ');\n';
+  var code = 'right(' + value_right + ');\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_left'] = function(block) {
   var value_left = Blockly.JavaScript.valueToCode(block, 'LEFT', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'turtle.left(' + value_left + ');\n';
+  var code = 'left(' + value_left + ');\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_width'] = function(block) {
   var value_width = Blockly.JavaScript.valueToCode(block, 'WIDTH', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'turtle.penWidth = ' + value_width + ';\n';
+  var code = 'penWidth( = )' + value_width + ');\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_colour'] = function(block) {
   var value_colour = Blockly.JavaScript.valueToCode(block, 'COLOUR', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'turtle.penStyle = ' + value_colour + ';\n';
+  var code = 'penStyle(' + value_colour + ');\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_getcolour'] = function(block) {
-  var code = 'turtle.penStyle';
+  var code = 'penStyle()';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['turtle_getwidth'] = function(block) {
-  var code = 'turtle.penWidth';
+  var code = 'penWidth()';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['turtle_savestate'] = function(block) {
-  var code = 'turtle.save();\n';
+  var code = 'save();\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_restorestate'] = function(block) {
-  var code = 'turtle.restore();\n';
+  var code = 'restore();\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_clear'] = function(block) {
-  var code = 'turtle.clear();\n';
+  var code = 'clear();\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_penup'] = function(block) {
-  var code = 'turtle.penUp();\n';
+  var code = 'penUp();\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_pendown'] = function(block) {
-  var code = 'turtle.penDown();\n';
+  var code = 'penDown();\n';
   return code;
 };
